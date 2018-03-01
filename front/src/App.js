9963f1d627b16b60026a2dea7b428b07f3b1b44e
Blake Guilloud <blake.guilloud@gmail.com>
2018-02-28 20:21:07 -0500

    Blake - update fork redirect location to this project instead of app.

--- front/src/App.js
+++ front/src/App.js
@@ -64,7 +64,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <a href="https://github.com/blakeGuilloud/scorekeeper"><img style={{ position: "absolute", top: 0, right: 0, border: 0, }} src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png" alt="Fork me on GitHub" /></a>
+        <a href="https://github.com/blakeGuilloud/scorekeeper-app"><img style={{ position: "absolute", top: 0, right: 0, border: 0, }} src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png" alt="Fork me on GitHub" /></a>
         <div className="jumbotron jumbotron-fluid">
           <div className="container">
             <h1 className="display-4">Scorekeeper</h1>
